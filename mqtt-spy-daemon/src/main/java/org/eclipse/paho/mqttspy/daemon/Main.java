c2e77f15fdfffd6d37b031fbef01ac048767daa0
Kamil Baczkowicz <kamil.baczkowicz@gmail.com>
2016-10-19 21:49:36 +0100

    Added new packages and main methods for starting mqtt-spy and mqtt-spy-daemon. #2

---
+++ mqtt-spy-daemon/src/main/java/org/eclipse/paho/mqttspy/daemon/Main.java
@@ -0,0 +1,28 @@
+/***********************************************************************************
+ * 
+ * Copyright (c) 2016 Kamil Baczkowicz
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ *
+ * The Eclipse Public License is available at
+ *    http://www.eclipse.org/legal/epl-v10.html
+ *    
+ * The Eclipse Distribution License is available at
+ *   http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ * 
+ *    Kamil Baczkowicz - initial API and implementation and/or initial documentation
+ *    
+ */
+package org.eclipse.paho.mqttspy.daemon;
+
+public class Main 
+{
+	public static void main(String[] args) 
+	{
+		pl.baczkowicz.mqttspy.daemon.Main.main(args);
+	}
+}
