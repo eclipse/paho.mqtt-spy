165181113481b23a39374ade215f1ca2605c6b81
Kamil Baczkowicz <kamil.baczkowicz@gmail.com>
2016-11-07 11:32:56 +0000

    Generic audit utils added.

---
+++ spy-common-ui/src/main/java/pl/baczkowicz/spy/ui/storage/MessageAuditUtils.java
@@ -0,0 +1,30 @@
+/***********************************************************************************
+ * 
+ * Copyright (c) 2016 Kamil Baczkowicz
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ *
+ * The Eclipse Public License is available at
+ *    http://www.eclipse.org/legal/epl-v10.html
+ *    
+ * The Eclipse Distribution License is available at
+ *   http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ * 
+ *    Kamil Baczkowicz - initial API and implementation and/or initial documentation
+ *    
+ */
+
+package pl.baczkowicz.spy.ui.storage;
+
+import pl.baczkowicz.spy.messages.FormattedMessage;
+
+public interface MessageAuditUtils
+{
+	String getCurrentMessageAsMessageLog(final BasicMessageStoreWithSummary<? extends FormattedMessage> store, final int messageIndex);
+	
+	String getAllMessagesAsMessageLog(final BasicMessageStoreWithSummary<? extends FormattedMessage> store);
+}
